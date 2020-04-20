50da3ee9b85f81d2a7d42fac3229445196b82f3f
Song jae-sang <thdwotkd@naver.com>
2020-04-20 22:55:39 +0900

    commit gradle and other things

--- src/java/main/business/helloWorld/Helloworld.java
+++ src/java/main/business/helloWorld/Helloworld.java
@@ -1,15 +1,14 @@
 package src.java.main.business.helloWorld;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Helloworld {
 
-    @GetMapping("/hello")
-    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "";
+    @GetMapping("/test")
+    public String ping() {
+        return "sdsd";
     }
 }
